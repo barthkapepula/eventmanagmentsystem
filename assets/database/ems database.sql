fa5ab01c986a1ffc6b59d374cfab4a997553235b
Bartholomew <65113211+barthkapepula@users.noreply.github.com>
2024-02-13 09:38:44 +0200

    Add files via upload

---
+++ assets/database/ems database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ems;
+USE ems;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
